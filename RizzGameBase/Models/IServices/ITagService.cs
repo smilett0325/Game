095b0770eba19998917ff98e012338c0d580771e
Hanlaoss <asd159632874@gmail.com>
2024-01-19 15:13:58 +0800

    Update Tag

--- RizzGameBase/Models/IServices/ITagService.cs
+++ RizzGameBase/Models/IServices/ITagService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RizzGameBase.Models.IServices
 {
-	internal interface ITagService
+	public interface ITagService
 	{
 	}
 }
--- RizzGameBase/Models/Services/TagService.cs
+++ RizzGameBase/Models/Services/TagService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using RizzGameBase.Models.IServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace RizzGameBase.Models.Services
 {
-	public class TagService
+	public class TagService:ITagService	
 	{
 	}
 }
